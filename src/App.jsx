b3deb39951023c698ad15edae96a027720e0f204
rajvichauhan77 <chauhanrajvi77@gmail.com>
2025-05-23 17:02:25 +0530

    srg

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Cart from './components/Cart'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+ 
+      <Cart/>
+
+    </>
+  )
+}
+
+export default App
